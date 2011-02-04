0e2bef421ad6092ae5912cc8b2ba4563d0614f48
Chris Blunt <chris@plymouthsoftware.com>
2011-02-04 13:59:43 +0000

    Initial Commit, configured with Mongoid and Rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinkWall::Application.config.secret_token = '365b81eab97ac353762ea14810f811ef2f736c7c57484411b445a6bd54626f728367d34fb35110ff7c03cf67d44f4bc82b0d45bea3f3d8289422157b58e63667'
