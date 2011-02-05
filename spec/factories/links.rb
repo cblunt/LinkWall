328fdf72dfd06f787cbed2f59dc71bf9cd082c1e
Chris Blunt <chris@plymouthsoftware.com>
2011-02-05 19:45:53 +0000

    Updated spec for 100% coverage

--- spec/factories/links.rb
+++ spec/factories/links.rb
@@ -1,5 +1,6 @@
 Factory.define :link do |l|
   l.url 'example.com'
+  l.association :user
 end
 
 Factory.define :invalid_link, :class => Link do |l|
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "creating from Twitter auth" do
+    it 'should create a new record from Twitter auth data' do
+      auth = {
+        'provider' => 'twitter',
+        'uid' => '1234',
+        'user_info' => { 'name' => 'Joe User' }
+      }
+
+      user = User.create_with_twitter_auth(auth)
+
+      user.name.should eql auth['user_info']['name']
+      user.auth_provider.should eql auth['provider']
+      user.auth_uid.should eql auth['uid']
+    end
+  end
 end
