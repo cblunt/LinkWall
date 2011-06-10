5d0363e822693d4a7535349061be9e67275ac864
Chris Blunt <chris@chrisblunt.com>
2011-06-10 08:11:20 +0000

    Code tweak tidy (test Cloud9 IDE)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user
-
+  
 protected
+
   def current_user
     @current_user ||= User.criteria.id(session[:user_id]).first if session[:user_id]
   end
